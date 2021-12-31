8311ae4c6eb7c7693376806c278268cc81e861b4
Kripa Sindhu <sindhukripa007@gmail.com>
2021-12-31 22:24:52 +0530

    I don't know why

---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"I am really not sure what I am doing now. I have just started using git and github today and still learning about it."<<endl;
+    cout<<"I have currently no one to talk to so feelinf a littile bored. Today is 31st dec, and I am lying on my bed."<<endl;
+    cout<<"But I am sure I will be the man I want to be one day.";
+    return 0;
+}
